919e5897507bebf80f8784230afc8bee09fd280a
gussyslove <gustavoa.castillo@correo.usa.edu.co>
2021-07-30 17:38:36 -0500

    ejemplo express

--- Node-express/index.js
+++ Node-express/index.js
@@ -1,10 +1,26 @@
 const express=require('express');
 const http=require('http');
 const morgan=require('morgan')
-
+const bodyparser=require('body-parser')
+const dishRouter=require("./routes/dishRouter")
 const app=express();
 app.use(morgan('dev'));
+app.use(bodyparser.json);
 
+app.use('/dishes',dishRouter)
+app.get('/dishes/:dishid',(req,res,next)=>{
+    res.end('will send the dish '+req.params.dishid)
+});
+app.post('/dishes/:dishid',(req,res,next)=>{
+    statusCode=403;
+    res.end('this operation is not supported '+req.params.dishid);
+});
+app.put('/dishes/:dishid',(req,res,next)=>{
+    res.end('will update the dish '+req.params.dishid);
+});
+app.delete('/dishes/:dishid',(req,res,next)=>{
+    res.end("will delete the dish "+req.params.dishid);
+});
 app.use(express.static(__dirname+'/public'));
 /*app.use((req,res,next)=>{
     console.log(req.headers);
